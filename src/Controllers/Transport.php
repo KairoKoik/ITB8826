896302b2566a81e6817fb7f210702697a5744d91
KKoik <kairo@koik.ee>
2018-10-22 21:37:41 +0300

    Function to see if "testconvertCartToArray" is working.

--- src/Controllers/Transport.php
+++ src/Controllers/Transport.php
@@ -15,5 +15,23 @@ class Transport
          json_decode($this->cart);
          return (json_last_error() == JSON_ERROR_NONE);
    }
+   public function convertCartToArray(){
+         $this->cart = array(
+                             "0" =>
+                             (object) [
+                               'id' => 1,
+                               'name' => 'A green door',
+                               'price' => 12.5,
+                               'tags' => 'living plant',
+                             ],
+                             1 =>
+                             (object)[
+                               'id' => 1,
+                               'name' => 'A green door',
+                               'price' => 12.5,
+                               'tags' => 'living plant',
+                             ],
+                           );
+   }
 
 }
