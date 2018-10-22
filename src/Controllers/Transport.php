d0619a746a2345f85593f1a9b6b557bfa6166f0d
KKoik <kairo@koik.ee>
2018-10-22 21:38:22 +0300

    Logic to "convertCartToArray" function.

--- src/Controllers/Transport.php
+++ src/Controllers/Transport.php
@@ -16,22 +16,7 @@ class Transport
          return (json_last_error() == JSON_ERROR_NONE);
    }
    public function convertCartToArray(){
-         $this->cart = array(
-                             "0" =>
-                             (object) [
-                               'id' => 1,
-                               'name' => 'A green door',
-                               'price' => 12.5,
-                               'tags' => 'living plant',
-                             ],
-                             1 =>
-                             (object)[
-                               'id' => 1,
-                               'name' => 'A green door',
-                               'price' => 12.5,
-                               'tags' => 'living plant',
-                             ],
-                           );
+         $this->cart = json_decode($this->cart);
    }
 
 }
